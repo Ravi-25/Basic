3b95a15d8b697a7eca794c90d71d77b1c383a2c5
STKS-033-RAVI\RAVI <pullagurlaravi@outlook.com>
2018-02-06 19:29:04 +0530

    EOD 06/02/18

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your name");
+            var Name = Console.ReadLine();
+            var Date = DateTime.Now;
+            Console.WriteLine($"Hello , {Name} on {Date:d} at {Date:t}!");
+            //Console.WriteLine(Date);
+            Console.WriteLine("Thats all");
+            Console.ReadLine();
+        }
+    }
+}
