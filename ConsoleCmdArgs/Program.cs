d3844f03f5e87cc53ac9816cba1c88163d2fbb86
STKS-033-RAVI\RAVI <pullagurlaravi@outlook.com>
2018-02-07 16:19:45 +0530

    2 07/02/18

---
+++ ConsoleCmdArgs/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleCmdArgs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //int ar = int.Parse(args[0]);
+            Console.WriteLine("Hello {0}",args[0]);
+        }
+    }
+}
