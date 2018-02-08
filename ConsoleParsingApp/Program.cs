1585d23480b39349f731cdfb35348440ef23feae
STKS-033-RAVI\RAVI <pullagurlaravi@outlook.com>
2018-02-08 10:52:48 +0530

    02 08/02/18

---
+++ ConsoleParsingApp/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleParsingApp
+{
+    class string1
+    {
+        static void Main(string[] args)
+        {
+            string a, b, c, s = "100";
+            bool success = false;
+            int i, o;
+            i = int.Parse(s);
+            Console.WriteLine(i);
+            a = "qwe";
+            success = int.TryParse(a, out o);
+            if (success)
+            {
+                Console.WriteLine(success + "  " + a);
+            }
+            else
+            {
+                Console.WriteLine("Not a valid integer " + success);
+            }
+
+        }
+    }
+
+    class charcasting
+    {
+        static void Main(string[] args)
+        {
+            //int n = 0;
+            for (int i = 0; i < (256); i++)
+            {
+                Console.Write((char)i + " ");
+            }
+
+        }
+    }
+}
