c5f0d4a9d79e89c8f5ef0c6398eefeac86cc2c1c
STKS-033-RAVI\RAVI <pullagurlaravi@outlook.com>
2018-02-08 10:02:48 +0530

    01 08/02/18

---
+++ ConsoleStringApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleStringApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "";
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < 100; i++)
+            {
+                sb.Append("a");
+                Console.WriteLine(sb.Length+" "+sb.Capacity);
+                str += "A";
+            }
+            Console.WriteLine(str);
+            Console.WriteLine(sb.ToString ());
+        }
+    }
+}
