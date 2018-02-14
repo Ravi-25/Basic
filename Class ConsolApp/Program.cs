0dd550d29c6aa8bc8081034d0c935f2e3ddfaa86
STKS-033-RAVI\RAVI <pullagurlaravi@outlook.com>
2018-02-14 14:55:39 +0530

    01 14/02/18

---
+++ Class ConsolApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_ConsolApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.EvenNum(30);
+        }
+        public void EvenNum(int num)
+        {
+            for (int i=0;i<=num;i+=2)
+            Console.WriteLine(" "+i+"");
+        }
+
+    }
+}
