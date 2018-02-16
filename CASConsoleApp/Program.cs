ae86a1fc5bff13471b410277a093aa8c36c626ab
STKS-033-RAVI\RAVI <pullagurlaravi@outlook.com>
2018-02-16 17:15:39 +0530

    16/02/18

---
+++ CASConsoleApp/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Security.Policy;
+using System.Reflection;
+
+namespace SecTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Load another assembly
+            Assembly asb = Assembly.LoadFrom(@"C:\Users\RAVI\Documents\Basic\InheritanceConsoleApp\bin\Debug\InheritanceConsoleApp.exe");
+
+            Console.WriteLine("***** Evidence Viewer *****\n");
+
+            if (asb != null)
+            {
+                DisplayEvidence(asb);
+            }
+            Console.ReadLine();
+        }
+
+        private static void DisplayEvidence(Assembly asm)
+        {
+            // Get evidence collection using enumerator.
+            Evidence e = asm.Evidence;
+            IEnumerator obj = e.GetHostEnumerator();
+            // Now print out the evidence.
+            while (obj.MoveNext())
+            {
+                Console.WriteLine(" **** Press Enter to continue ****");
+                Console.ReadLine();
+                Console.WriteLine(obj.Current);
+            }
+        }
+    }
+}
---
+++ ExtensionMethodsConsoleApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Program.cs
+public static class EMClass
+{
+    public static int ToInt32Ext(this string s)
+    {
+        return Int32.Parse(s);
+    }
+    public static int ToInt32Static(string s)
+    {
+        return Int32.Parse(s);
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        string s = "9";
+        int i = s.ToInt32Ext(); // LINE A
+        Console.WriteLine(i);
+        int j = EMClass.ToInt32Static(s); // LINE B
+        Console.WriteLine(j);
+        Console.ReadLine();
+    }
+}
